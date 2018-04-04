6f429156f7393a519c6fbb2dff59d8dc80dacdbb
e-a-n <ean.director@gmail.com>
2018-04-03 21:39:20 -0700

    setup html render view

--- sampleApp/app.js
+++ sampleApp/app.js
@@ -1,1 +1,28 @@
-console.log("Hello World!!");
+const main = function() {
+
+    const guiReference = "graphicsDisposition";
+    const div = document.getElementById(guiReference);
+    const mapAttributes = (element, ...args) => {
+        args.forEach((A) => {
+            element.setAttribute(A[0], A[1]);
+        });
+    }
+
+
+    const canvas = document.createElement("canvas");
+    const canvasAttributes = [
+        ["width", 200],
+        ["height", 300],
+        ["id", "glCanvas"]
+    ];
+
+    canvas.innerHTML = "Hello World!!";
+    div.appendChild(canvas);
+    mapAttributes(canvas, ...canvasAttributes);
+
+
+    const GL = canvas.getContext("webgl");
+    console.log(guiReference,canvas,GL);
+}
+
+window.onload = main;
